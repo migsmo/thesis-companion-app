e642568f30e27a33b27c6e0650d793e5bd68f9b6
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2023-07-22 11:08:01 +0800

    added responsive icons

--- src/pages/workouts/index.tsx
+++ src/pages/workouts/index.tsx
@@ -1,5 +1,6 @@
 import ImageCards from "@/components/image-cards/image-cards";
 import {
+  Box,
   Container,
   Grid,
   Group,
@@ -7,13 +8,14 @@ import {
   Title,
   UnstyledButton,
   createStyles,
+  Image,
 } from "@mantine/core";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import Exercises from "../../../data/exercises.json"
+import Exercises from "../../../data/exercises.json";
 
-const exercises = Exercises
+const exercises = Exercises;
 
 const useStyles = createStyles((theme: MantineTheme) => ({
   root: {
@@ -43,6 +45,7 @@ const useStyles = createStyles((theme: MantineTheme) => ({
     flexDirection: "column",
     textAlign: "center",
     cursor: "pointer",
+    justifyContent: "center",
   },
   title: {
     opacity: "0.800000011920929",
@@ -54,8 +57,24 @@ const useStyles = createStyles((theme: MantineTheme) => ({
     marginRight: "auto",
     marginBottom: "5px",
   },
-  test: {
-    backgroundImage: "url(./ellipsis.png);",
+  selectedIcon: {
+    width: "60px",
+    height: "60px",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  selected: {
+    width: "140px",
+    height: "140px",
+    background:
+      "linear-gradient(137deg, #0085FF 0%, #00278B 100%), linear-gradient(180deg, #D9D9D9 0%, rgba(217, 217, 217, 0.00) 100%)",
+    borderRadius: "100px",
+    filter: "drop-shadow(0px 0px 13px rgba(54, 205, 233, 0.20))",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1,
+    marginBottom: "80px",
   },
 }));
 
@@ -93,26 +112,78 @@ function Workouts() {
             .filter((exercise) => exercise.target === target)
             .map((exercise, index) => (
               <Grid.Col xl={3} sm={4} xs={6} key={index}>
-                <ImageCards image="/elbow-planks.png" title={exercise.name} link={exercise.link} />
+                <ImageCards
+                  image="/elbow-planks.png"
+                  title={exercise.name}
+                  link={exercise.link}
+                />
               </Grid.Col>
             ))}
         </Grid>
       </Container>
 
-      <div className={classes.background}>
-        <div onClick={() => setTarget("Upper Body")} className={classes.target}>
-          <img src="./upper-icon.png" className={classes.icon} />
-          <div className={classes.title}>UPPER BODY</div>
-        </div>
-        <div onClick={() => setTarget("Core")} className={classes.target}>
-          <img src="./abs-icon.png" className={classes.icon} />
-          <div className={classes.title}>CORE</div>
-        </div>
-        <div onClick={() => setTarget("Lower Body")} className={classes.target}>
-          <img src="./lower-icon.png" className={classes.icon} />
-          <div className={classes.title}>LOWER BODY</div>
-        </div>
-      </div>
+      <Box className={classes.background}>
+        {target === "Upper Body" ? (
+          <Box
+            className={classes.selected}
+            w={{ base: "100px", sm: "140px" }}
+            h={{ base: "100px", sm: "140px" }}
+          >
+            <Box
+              onClick={() => setTarget("Upper Body")}
+              className={classes.target}
+            >
+              <img src="./upper-icon.png" className={classes.selectedIcon} />
+            </Box>
+          </Box>
+        ) : (
+          <Box
+            onClick={() => setTarget("Upper Body")}
+            className={classes.target}
+          >
+            <img src="./upper-icon.png" className={classes.icon} />
+            <Box className={classes.title}>UPPER BODY</Box>
+          </Box>
+        )}
+        {target === "Core" ? (
+          <Box
+            className={classes.selected}
+            w={{ base: "100px", sm: "140px" }}
+            h={{ base: "100px", sm: "140px" }}
+          >
+            <Box onClick={() => setTarget("Core")} className={classes.target}>
+              <img src="./abs-icon.png" className={classes.selectedIcon} />
+            </Box>
+          </Box>
+        ) : (
+          <Box onClick={() => setTarget("Core")} className={classes.target}>
+            <img src="./abs-icon.png" className={classes.icon} />
+            <Box className={classes.title}>CORE</Box>
+          </Box>
+        )}
+        {target === "Lower Body" ? (
+          <Box
+            className={classes.selected}
+            w={{ base: "100px", sm: "140px" }}
+            h={{ base: "100px", sm: "140px" }}
+          >
+            <Box
+              onClick={() => setTarget("Lower Body")}
+              className={classes.target}
+            >
+              <img src="./lower-icon.png" className={classes.selectedIcon} />
+            </Box>
+          </Box>
+        ) : (
+          <Box
+            onClick={() => setTarget("Lower Body")}
+            className={classes.target}
+          >
+            <img src="./lower-icon.png" className={classes.icon} />
+            <Box className={classes.title}>LOWER BODY</Box>
+          </Box>
+        )}
+      </Box>
     </>
   );
 }
